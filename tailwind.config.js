ffdcc34356d0815c109b0ac0520cfc2ee8ba240f
vrej.sanati <vrej.sanati@munchkin.com>
2024-11-08 15:57:05 -0800

    fixing the tailwind and cleaning up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './**/*.php',
-    './src/**/*.{html,js,php}',
-    './inc/components/**/*.php',],
+    "./**/*.php",
+    "./src/**/*.{html,js,php}",
+    "./inc/components/**/*.php",
+  ],
   theme: {
     extend: {
       colors: {
-        primary: '#444444', // custom primary color
-        secondary: '#fbbf24', // custom secondary color
+        primary: "#444444", // custom primary color
+        secondary: "#fbbf24", // custom secondary color
       },
       backgroundImage: {
-        'white-to-transparent': 'linear-gradient(to right, rgba(255, 255, 255, 0.98) 75%, transparent 100%)',
+        "white-to-transparent":
+          "linear-gradient(to right, rgba(255, 255, 255, 0.98) 75%, transparent 100%)",
       },
     },
+    plugins: [],
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
-
+};
